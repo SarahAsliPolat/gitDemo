b7bb0532207637f038981653c5f4b3f2d095563d
AsliPolat <sarahaslipolat@gmail.com>
2020-08-01 12:08:38 -0400

    update HappyB20.java

--- src/demo/HappyB20.java
+++ src/demo/HappyB20.java
@@ -3,6 +3,7 @@ package demo;
 public class HappyB20 {
     public static void main(String[] args) {
         System.out.println("bugun bayram");
+        System.out.println("misafir bekliyoruz");
 
 
     }
