3e6b7110267e8b7f74ad448e5177140ed4062d3f
SarahAsliPolat <asliatamanpolat@gmail.com>
2020-11-09 13:06:33 -0500

    hellooo

--- src/hello.java
+++ src/hello.java
@@ -1,5 +1,5 @@
 public class hello {
     public static void main(String[] args) {
-        System.out.println("how ar you");
+        System.out.println("how are you");
     }
 }
---
+++ src/recap.java
@@ -0,0 +1,6 @@
+public class recap {
+    public static void main(String[] args) {
+
+        System.out.println("it is getting better and faster");
+    }
+}
