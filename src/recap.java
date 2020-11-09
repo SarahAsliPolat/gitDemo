2532f079fcc7ab236316d4f78c0d5cbdd8e5ce35
SarahAsliPolat <asliatamanpolat@gmail.com>
2020-11-09 13:19:06 -0500

    update recap.java

--- src/recap.java
+++ src/recap.java
@@ -2,5 +2,6 @@ public class recap {
     public static void main(String[] args) {
 
         System.out.println("it is getting better and faster");
+        System.out.println("faster and better!!");
     }
 }
